74423232166bfcea8f25608b48d530cb60261e2f
Alex <148038194+Einfachalf@users.noreply.github.com>
2024-01-21 13:55:28 +0100

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,7 +5,7 @@ import { OpenAIStream, StreamingTextResponse } from 'ai';
 // but configure it to point to fireworks.ai
 const fireworks = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || '',
-  baseURL: 'https://api.fireworks.ai/inference/v1',
+  baseURL: 'https://api.fireworks.ai/inference/v1/chat/completions',
 });
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
@@ -16,10 +16,15 @@ export async function POST(req: Request) {
   // Ask Fireworks for a streaming chat completion using Llama 2 70b model
   // @see https://app.fireworks.ai/models/fireworks/llama-v2-70b-chat
   const response = await fireworks.chat.completions.create({
-    model: 'accounts/fireworks/models/llama-v2-70b-chat',
+    model: 'accounts/fireworks/models/mixtral-8x7b-instruct',
     stream: true,
-    max_tokens: 1000,
+    max_tokens: 4000,
     messages,
+    stop: [
+        "<|im_start|>",
+        "<|im_end|>",
+        "<|endoftext|>"
+    ],
   });
   // Convert the response into a friendly text-stream.
   const stream = OpenAIStream(response);
